abaac16fa699f8f14d75f61f3a220d687a537012
小怪兽 <wff_1994@126.com>
2019-03-25 23:17:47 +0800

    资源更新

---
+++ Assets/Scripts/Share/ScrollUV.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollUV : MonoBehaviour
+{
+
+    public float speedX = 0.5f;
+    public float speedY = 0.5f;
+    private Material mat;
+
+    // Use this for initialization
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mat != null)
+        {
+            var offsetX = Time.time * speedX;
+            var offsetY = Time.time * speedY;
+            mat.mainTextureOffset = new Vector2(offsetX, offsetY);
+        }
+    }
+}
